d9bd06eaf381aace6aa4c2caec1edcbd1fc0e805
Ramil <ramil-urazgulov444@mail.ru>
2022-11-23 17:59:31 +0700

    Создание метода

--- Zadacha/Program.cs
+++ Zadacha/Program.cs
@@ -1,6 +1,17 @@
 ﻿string[] array1 = new string[5] {"444", "23", "helloWorld", "dsa", "332"};
 string[] array2 = new string[array1.Length];
-
+void SecondArray(string[] array1, string[] array2)
+{
+    int count = 0;
+    for (int i = 0; i < array1.Length; i++)
+    {
+    if(array1[i].Length <= 3)
+        {
+        array2[count] = array1[i];
+        count++;
+        }
+    }
+}
 
 
         
